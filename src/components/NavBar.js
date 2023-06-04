c1c01e5e498c9f5d5c045a2825471c73b247d2f5
Ian Riera <ian.riera.smolinska@gmail.com>
2023-06-04 18:59:23 +0200

    feat: added hover effect in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,25 +2,42 @@ import { Nanum_Gothic_Coding } from "next/font/google";
 import Link from "next/link";
 import React from "react";
 import Logo from './Logo';
+import { useRouter } from "next/router";
 
+
+const CustomLink = ({ href, title, className = "" }) => {
+    const router = useRouter();
+    return (
+        <Link href={href} className={`${className} relative group`}>
+            {title}
+            <span className={`h-[1px] inline-block bg-dark 
+            absolute left-0 bottom-0.5
+            group-hover:w-full transition-[width] ease duration-300
+            ${router.asPath === href ? "w-full" : "w-0"}`}            
+            >&nbsp;
+            </span>
+        </Link>
+    );
+};
 const NavBar = () => {
     return (
         <header
-        className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+            className='w-full px-32 py-8 font-medium flex items-center justify-between'>
             <nav>
-                <Link href='/'>Home</Link>
-                <Link href='/about'>About</Link>
-                <Link href='/projects'>Projects</Link>
-                <Link href='/blog'>Blog</Link>
-                <Link href='/contact'>Contact</Link>
+                <CustomLink href='/' title="Home" className="mr-4"/>
+                <CustomLink href='/about' title="About" className="mx-4"/>
+                <CustomLink href='/projects' title="Projects" className="mx-4" />
+                <CustomLink href='/blog' title="Blog" className="ml-4"/>
             </nav>
-            <Logo />
+
             <nav>
                 <Link href='/' target={"_blank"}>L</Link>
                 <Link href='/' target={"_blank"}>G</Link>
             </nav>
-
-            </header>
+            <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+                <Logo />
+            </div>
+        </header>
     );
 };
 
