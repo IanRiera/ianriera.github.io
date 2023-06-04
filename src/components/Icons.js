123c0c5fb0d98fd296fd8e2ba463202e3cbd8bf2
Ian Riera <ian.riera.smolinska@gmail.com>
2023-06-04 20:25:08 +0200

    feat: create icons

---
+++ src/components/Icons.js
@@ -0,0 +1,373 @@
+import * as React from "react"
+
+export const LinkedInIcon = ({className, ...rest}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 256 256"
+    className={`w-full h-auto ${className}`}
+    {...rest}
+  >
+    <path fill="rgba(255, 255, 255, 0)" d="M0 0h256v256H0z" />
+    <g fill="none">
+      <rect width={256} height={256} fill="#fff" rx={60} />
+      <rect width={256} height={256} fill="#0A66C2" rx={60} />
+      <path
+        fill="#fff"
+        d="M184.715 217.685h29.27a4 4 0 0 0 4-3.999l.015-61.842c0-32.323-6.965-57.168-44.738-57.168-14.359-.534-27.9 6.868-35.207 19.228a.32.32 0 0 1-.595-.161V101.66a4 4 0 0 0-4-4h-27.777a4 4 0 0 0-4 4v112.02a4 4 0 0 0 4 4h29.268a4 4 0 0 0 4-4v-55.373c0-15.657 2.97-30.82 22.381-30.82 19.135 0 19.383 17.916 19.383 31.834v54.364a4 4 0 0 0 4 4ZM38 59.627c0 11.865 9.767 21.627 21.632 21.627 11.862-.001 21.623-9.769 21.623-21.631C81.253 47.761 71.491 38 59.628 38 47.762 38 38 47.763 38 59.627Zm6.959 158.058h29.307a4 4 0 0 0 4-4V101.66a4 4 0 0 0-4-4H44.959a4 4 0 0 0-4 4v112.025a4 4 0 0 0 4 4Z"
+      />
+    </g>
+  </svg>
+);
+
+export const GithubIcon = ({className, ...rest}) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 512 512"
+      className={`w-full h-auto ${className}`}
+      {...rest}
+    >
+      <path fill="rgba(255, 255, 255, 0)" d="M0 0h512v512H0z" />
+      <path
+        fill="currentColor"
+        d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9a17.56 17.56 0 0 0 3.8.4c8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1a102.4 102.4 0 0 1-22.6 2.7c-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1a63 63 0 0 0 25.6-6c2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8a18.64 18.64 0 0 1 5-.5c8.1 0 26.4 3.1 56.6 24.1a208.21 208.21 0 0 1 112.2 0c30.2-21 48.5-24.1 56.6-24.1a18.64 18.64 0 0 1 5 .5c12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5a19.35 19.35 0 0 0 4-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32Z"
+      />
+    </svg>
+);
+
+export const GmailIcon = ({className, ...rest}) => (
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    width={800}
+    height={800}
+    viewBox="0 0 512 512"
+    className={`w-full h-auto ${className}`}
+    {...rest}
+  >
+    <circle
+      cx={256}
+      cy={256}
+      r={256}
+      style={{
+        fill: "#263a7a",
+      }}
+    />
+    <path
+      d="M396.062 148.928 118.28 335.858l173.502 173.64c121.637-17.015 215.866-119.491 220.053-244.796L396.062 148.928z"
+      style={{
+        fill: "#121149",
+      }}
+    />
+    <path
+      d="M131.489 142.222h249.02c11.867 0 21.488 9.621 21.488 21.488v155.198c0 11.867-9.621 21.488-21.488 21.488h-20.225v47.107c0 9.206-11.13 13.815-17.637 7.306l-54.413-54.413H131.491c-11.867 0-21.488-9.621-21.488-21.488V163.711c-.002-11.868 9.619-21.489 21.486-21.489z"
+      style={{
+        fill: "#ffedb5",
+      }}
+    />
+    <path
+      d="M401.998 163.711c0-11.867-9.621-21.488-21.488-21.488H255.426v198.175h32.808l54.413 54.413c6.509 6.509 17.637 1.9 17.637-7.306v-47.107h20.225c11.867 0 21.488-9.621 21.488-21.488V163.711z"
+      style={{
+        fill: "#fee187",
+      }}
+    />
+    <path
+      d="M342.647 394.811c6.509 6.509 17.637 1.9 17.637-7.306v-47.107h20.225c10.692 0 19.535-7.818 21.187-18.044L256 227.556l-145.698 94.798c1.652 10.226 10.495 18.044 21.187 18.044h156.743l54.415 54.413z"
+      style={{
+        fill: "#fee187",
+      }}
+    />
+    <path
+      d="m256 227.556-.574.374v112.468h32.808l54.413 54.413c6.509 6.509 17.637 1.9 17.637-7.306v-47.107h20.225c10.692 0 19.535-7.818 21.187-18.044L256 227.556z"
+      style={{
+        fill: "#ffc91b",
+      }}
+    />
+    <path
+      d="M116.891 147.973 256 256l139.107-108.027c-3.834-3.558-8.954-5.751-14.598-5.751h-249.02c-5.642 0-10.762 2.193-14.598 5.751z"
+      style={{
+        fill: "#ffc61b",
+      }}
+    />
+    <path
+      d="M395.107 147.973c-3.834-3.558-8.954-5.751-14.598-5.751H255.426v113.331L256 256l139.107-108.027z"
+      style={{
+        fill: "#d48b07",
+      }}
+    />
+  </svg>
+);
+
+export const MoonIcon = ({className, ...rest}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    className={`w-full h-auto ${className}`}
+    {...rest}
+  >
+    <path fill="rgba(255, 255, 255, 0)" d="M0 0h24v24H0z" />
+    <g>
+      <animateTransform
+        fill="none"
+        stroke="currentColor"
+        strokeDasharray={2}
+        strokeDashoffset={2}
+        strokeLinecap="round"
+        strokeWidth={2}
+        attributeName="transform"
+        dur="30s"
+        repeatCount="indefinite"
+        type="rotate"
+        values="0 12 12;360 12 12"
+      />
+    </g>
+    <g fill="currentColor">
+      <path d="m15.22 6.03 2.53-1.94L14.56 4 13.5 1l-1.06 3-3.19.09 2.53 1.94-.91 3.06 2.63-1.81 2.63 1.81-.91-3.06Z">
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          dur="0.4s"
+          values="1;0"
+        />
+      </path>
+      <path d="M19.61 12.25 21.25 11l-2.06-.05L18.5 9l-.69 1.95-2.06.05 1.64 1.25-.59 1.98 1.7-1.17 1.7 1.17-.59-1.98Z">
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="0.2s"
+          dur="0.4s"
+          values="1;0"
+        />
+      </path>
+    </g>
+    <g
+      fill="currentColor"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+    >
+      <path d="M7 6a10.994 10.994 0 0 0 12.56 10.89C17.95 19.36 15.17 21 12 21a9 9 0 0 1-9-9c0-3.17 1.64-5.95 4.11-7.56C7.04 4.95 7 5.47 7 6Z" />
+      <set attributeName="opacity" begin="0.6s" to={0} />
+    </g>
+    <mask id="a">
+      <circle cx={12} cy={12} r={12} fill="#fff" />
+      <circle cx={18} cy={6} r={12} fill="#fff">
+        <animate
+          fill="freeze"
+          attributeName="cx"
+          begin="0.6s"
+          dur="0.4s"
+          values="18;22"
+        />
+        <animate
+          fill="freeze"
+          attributeName="cy"
+          begin="0.6s"
+          dur="0.4s"
+          values="6;2"
+        />
+        <animate
+          fill="freeze"
+          attributeName="r"
+          begin="0.6s"
+          dur="0.4s"
+          values="12;3"
+        />
+      </circle>
+      <circle cx={18} cy={6} r={10}>
+        <animate
+          fill="freeze"
+          attributeName="cx"
+          begin="0.6s"
+          dur="0.4s"
+          values="18;22"
+        />
+        <animate
+          fill="freeze"
+          attributeName="cy"
+          begin="0.6s"
+          dur="0.4s"
+          values="6;2"
+        />
+        <animate
+          fill="freeze"
+          attributeName="r"
+          begin="0.6s"
+          dur="0.4s"
+          values="10;1"
+        />
+      </circle>
+    </mask>
+  </svg>
+);
+
+export const SunIcon = ({className, ...rest}) => (
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <g
+      fill="none"
+      stroke="currentColor"
+      strokeDasharray={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+    >
+      <path d="M12 21v1m9-10h1M12 3V2M3 12H2">
+        <animate
+          fill="freeze"
+          attributeName="stroke-dashoffset"
+          dur="0.2s"
+          values="4;2"
+        />
+      </path>
+      <path d="m18.5 18.5.5.5m-.5-13.5L19 5m-13.5.5L5 5m.5 13.5L5 19">
+        <animate
+          fill="freeze"
+          attributeName="stroke-dashoffset"
+          begin="0.2s"
+          dur="0.2s"
+          values="4;2"
+        />
+      </path>
+    </g>
+    <g fill="none" fillOpacity={0}>
+      <path d="m15.22 6.03 2.53-1.94L14.56 4 13.5 1l-1.06 3-3.19.09 2.53 1.94-.91 3.06 2.63-1.81 2.63 1.81z">
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="0.6s;lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+6s"
+          dur="0.4s"
+          values="0;1"
+        />
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+2.2s"
+          dur="0.4s"
+          values="1;0"
+        />
+      </path>
+      <path d="M13.61 5.25 15.25 4l-2.06-.05L12.5 2l-.69 1.95L9.75 4l1.64 1.25-.59 1.98 1.7-1.17 1.7 1.17z">
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+3s"
+          dur="0.4s"
+          values="0;1"
+        />
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+5.2s"
+          dur="0.4s"
+          values="1;0"
+        />
+      </path>
+      <path d="M19.61 12.25 21.25 11l-2.06-.05L18.5 9l-.69 1.95-2.06.05 1.64 1.25-.59 1.98 1.7-1.17 1.7 1.17z">
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+0.4s"
+          dur="0.4s"
+          values="0;1"
+        />
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+2.8s"
+          dur="0.4s"
+          values="1;0"
+        />
+      </path>
+      <path d="m20.828 9.731 1.876-1.439-2.366-.067L19.552 6l-.786 2.225-2.366.067 1.876 1.439L17.601 12l1.951-1.342L21.503 12z">
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+3.4s"
+          dur="0.4s"
+          values="0;1"
+        />
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="lineMdSunnyFilledLoopToMoonFilledLoopTransition0.begin+5.6s"
+          dur="0.4s"
+          values="1;0"
+        />
+      </path>
+    </g>
+    <mask id="a">
+      <circle cx={12} cy={12} r={12} fill="#fff" />
+      <circle cx={22} cy={2} r={3} fill="#fff">
+        <animate
+          fill="freeze"
+          attributeName="cx"
+          begin="0.1s"
+          dur="0.4s"
+          values="22;18"
+        />
+        <animate
+          fill="freeze"
+          attributeName="cy"
+          begin="0.1s"
+          dur="0.4s"
+          values="2;6"
+        />
+        <animate
+          fill="freeze"
+          attributeName="r"
+          begin="0.1s"
+          dur="0.4s"
+          values="3;12"
+        />
+      </circle>
+      <circle cx={22} cy={2} r={1}>
+        <animate
+          fill="freeze"
+          attributeName="cx"
+          begin="0.1s"
+          dur="0.4s"
+          values="22;18"
+        />
+        <animate
+          fill="freeze"
+          attributeName="cy"
+          begin="0.1s"
+          dur="0.4s"
+          values="2;6"
+        />
+        <animate
+          fill="freeze"
+          attributeName="r"
+          begin="0.1s"
+          dur="0.4s"
+          values="1;10"
+        />
+      </circle>
+    </mask>
+    <circle cx={12} cy={12} r={6} fill="currentColor" mask="url(#a)">
+      <set attributeName="opacity" begin="0.5s" to={0} />
+      <animate
+        fill="freeze"
+        attributeName="r"
+        begin="0.1s"
+        dur="0.4s"
+        values="6;10"
+      />
+    </circle>
+  </svg>
+);
+
+
+  
