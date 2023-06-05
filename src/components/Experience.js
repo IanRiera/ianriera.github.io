185453aa37cd26ff5ada1a076111cc20a7b6933f
Ian Riera <ian.riera.smolinska@gmail.com>
2023-06-05 16:06:32 +0200

    feat: created Experience component

---
+++ src/components/Experience.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Experience = () => {
+    return (
+        <div>
+            <h2 className="font-bold text-8xl mt-64 w-full text-center">Experience</h2>
+        </div>
+    );
+}
+
+export default Experience;
