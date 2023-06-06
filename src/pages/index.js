971971f8fd96de94dd09fad2e7161321ac181591
Ian Riera <ian.riera.smolinska@gmail.com>
2023-06-07 00:49:16 +0200

    feat: added className to img

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,8 @@ export default function Home() {
         <Layout className="pt-0">
           <div className="flex items-center justify-between w-full">
             <div className="w-1/2">
-              <Image src={profilePic} alt="IanRiera" className='w-full h-auto' />
+              <Image src={profilePic} alt="IanRiera" className='w-full
+              h-auto max-w-sm rounded-lg shadow-none transition-shadow duration-300 ease-in-out hover:shadow-lg hover:shadow-black/30' />
             </div>
             <div className="w-1/2 flex flex-col items-center self-center">
               <AnimatedText text="A Computer Vision Engineer With Multiple Hats."
