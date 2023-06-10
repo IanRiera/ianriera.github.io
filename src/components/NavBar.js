72868f4db731ab709adcbbf5a887e382ef6992e8
Ian Riera <ian.riera.smolinska@gmail.com>
2023-06-10 20:12:05 +0200

    fix: moon sun icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -52,21 +52,21 @@ const NavBar = () => {
                 <motion.a href='mailto:ian.riera.smolinska@gmail.com' target={"_blank"}
                     whileHover={{ y: -2 }}
                     whileTap={{ scale: 0.9 }}
-                    className="w-6 ml-3">
+                    className="w-6 mx-3">
                     <GmailIcon />
                 </motion.a>
-            <button
+                <button
             onClick={() => setMode(mode === "light" ? "dark" : "light" )}
-            className={`ml-3 flex items-center justify-center rounded-full 
+            className={`w-6 ml-3 flex items-center justify-center rounded-full 
             p-1 ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}`}
-            >
+                >                    
             {
                 mode === "dark" ?
                     <SunIcon className={"fill-dark"}/>
                     : <MoonIcon className={"fill-dark"}/>
             }
 
-            </button>
+                </button>
 
 
             </nav>
